6020bab0ac99aeec9dfda1f46a5c926f37ef1201
unknown <tasnimtanzim55@gmail.com>
2022-05-19 04:20:20 +0600

    Delete Api added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -31,6 +31,21 @@ async function run() {
             const result = await taskCollection.find().toArray();
             res.send(result);
         })
+         // GET by ID 
+         app.get('/tasks/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await taskCollection.findOne(query);
+            res.send(result);
+        })
+        // Delete 
+        app.delete('/tasks/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await taskCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
     }
     finally {
