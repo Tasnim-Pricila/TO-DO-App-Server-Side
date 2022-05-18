8c7d6c6b46ed3f4937a0f20fabd9f0970c2ebfc5
unknown <tasnimtanzim55@gmail.com>
2022-05-19 03:32:52 +0600

    Post api added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,6 +11,27 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send("To-do-app");
 })
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.u6vmp.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const taskCollection = client.db('To-Do-App').collection('tasks');
+
+        // POST 
+        app.post('/tasks', async (req, res) => {
+            const task = req.body;
+            const result = await taskCollection.insertOne(task);
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
     console.log("Listening to port", port);
